ed56e39bc61cd16f2505e5aaa930399a0ad8b6ca
yangyao <yangyaohappy@outlook.com>
2018-12-19 12:58:53 +0800

    fix flutter bug when upload pic with utf8 request body

--- lib/network_utils.dart
+++ lib/network_utils.dart
@@ -1,11 +1,10 @@
-library network_utils;
-
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_app/common/functions/getToken.dart';
+import 'package:flutter_app/common/functions/saveUserToken.dart';
 import 'package:flutter/material.dart';
-import 'cache_utils.dart';
-import 'dialog_utils.dart';
+import 'package:flutter_app/common/functions/showDialogSingleButton.dart';
 
 class NetworkUtil {
   // 单例模式
@@ -16,16 +15,17 @@ class NetworkUtil {
   Future<dynamic> get(BuildContext context, String url) async {
     Map<String, String> headers = {};
 
-    await getCache('jwt-token').then((result) {
+    await getToken().then((result) {
       headers['Authorization'] = result;
     });
 
     try {
       final response = await http.get(url, headers: headers);
       final int statusCode = response.statusCode;
+      print("请求接口${url}，请求头${headers},响应头${response.headers}，响应结果${response.body}");
       // 如果包含Token ，那么更新TOKEN
-      if (response.headers.containsKey('Authorization')) {
-        setCache('jwt-token',response.headers['Authorization']);
+      if (response.headers.containsKey('authorization')) {
+        saveUserToken(response.headers['authorization']);
       }
       // 如果Token 过期，强制跳转到登陆页面
       if (statusCode == 401) {
@@ -38,7 +38,6 @@ class NetworkUtil {
         throw Exception("网络500错误,错误信息：${response.body}，请联系开发人员处理");
       }
       if (statusCode == 200) {
-        print("请求接口${url}，请求头${headers},请求结果${response.body}");
         return json.decode(response.body);
       }
       throw new Exception("网络异常:(HTTP:${statusCode})，请检查网络后重试！");
@@ -50,7 +49,7 @@ class NetworkUtil {
 
   Future<dynamic> post(BuildContext context, String url,
       {Map headers, body, encoding}) async {
-    await getCache('jwt-token').then((result) {
+    await getToken().then((result) {
       headers['Authorization'] = result;
     });
 
@@ -58,9 +57,10 @@ class NetworkUtil {
       final response = await http.post(url,
           body: body, headers: headers, encoding: encoding);
       final int statusCode = response.statusCode;
+      print("请求接口${url}，请求头${headers}，请求body:${body}，响应头${response.headers}，响应结果${response.body}");
       // 如果包含Token ，那么更新TOKEN
-      if (response.headers.containsKey('Authorization')) {
-        setCache('jwt-token',response.headers['Authorization']);
+      if (response.headers.containsKey('authorization')) {
+        saveUserToken(response.headers['authorization']);
       }
       // 如果Token 过期，强制跳转到登陆页面
       if (statusCode == 401) {
@@ -73,7 +73,6 @@ class NetworkUtil {
         throw Exception("网络500错误,错误信息：${response.body}，请联系开发人员处理");
       }
       if (statusCode == 200) {
-        print("请求接口${url}，请求头${headers}，请求body:${body}，请求结果${response.body}");
         return json.decode(response.body);
       }
       throw new Exception("网络异常:(HTTP:${statusCode})，请检查网络后重试！");
@@ -86,10 +85,18 @@ class NetworkUtil {
   Future<dynamic> upload(
       BuildContext context, String url, List<http.MultipartFile > imageFileList,
       {Map headers, body, encoding}) async {
-    await getCache('jwt-token').then((result) {
+    await getToken().then((result) {
       headers['Authorization'] = result;
     });
+
+    // 增加body 字段 (使用很挫的方式，因为field加入会报错)
+    url = url + "?1=1";
+    body.forEach((k, v) {
+      url = url+"&${k}=${v}";
+    });
+
     var uri = Uri.parse(url);
+
     var request = new http.MultipartRequest("POST", uri);
 
     // 添加需要上传的图片
@@ -97,9 +104,9 @@ class NetworkUtil {
       request.files.add(imageFile);
     });
 
-    // 增加body 字段
+    // 增加body 字段，这里有bug
     body.forEach((k, v) {
-      request.fields[k] = v;
+      //request.fields[k] = v;
     });
     // 增加header字段
     headers.forEach((k, v) {
@@ -112,8 +119,8 @@ class NetworkUtil {
           .decodeStream(responseStream.stream);
       final int statusCode = responseStream.statusCode;
       // 如果包含Token ，那么更新TOKEN
-      if (responseStream.headers.containsKey('Authorization')) {
-        setCache('jwt-token',responseStream.headers['Authorization']);
+      if (responseStream.headers.containsKey('authorization')) {
+        saveUserToken(responseStream.headers['authorization']);
       }
       // 如果Token 过期，强制跳转到登陆页面
       if (statusCode == 401) {
